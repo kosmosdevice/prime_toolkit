2b78f952c7559482c65f8241352312d686f43b32
Karl Walter <karl.walter@hotmail.com>
2026-03-25 11:01:53 +0100

    feat: palindromic primes

--- prime_toolkit.c
+++ prime_toolkit.c
@@ -25,6 +25,8 @@ typedef struct {
 } int_tuple_list;
 
 bool test_prime(int n) {
+	if (n == 1)
+		return false;
 	for (int i = 2; i <= floor(sqrt(n)); i++) {
 		if (n % i == 0)
 			return false;
@@ -34,8 +36,8 @@ bool test_prime(int n) {
 
 int_list primes_up_to(int n) {
 	int_list result = {0};
-    bool *prime = malloc(n * sizeof(bool));
-    memset(prime, true, n * sizeof(bool));
+	bool *prime = malloc(n + 1 * sizeof(bool));
+	memset(prime, true, (n + 1) * sizeof(bool));
 
 	for (int p = 2; p * p <= n; p++) {
 		if (prime[p] == true) {
@@ -53,47 +55,50 @@ int_list primes_up_to(int n) {
 			result.data[result.size++] = i;
 		}
 	}
-    free(prime);
+	free(prime);
 	return result;
 }
 
 int_list sieve_of_atkin(int n) {
-    int_list result = {0};
-    result.data = malloc(n * sizeof(int));
+	int_list result = {0};
+	result.data = malloc(n * sizeof(int));
 	result.size = 0;
-    bool *prime = malloc(n * sizeof(bool));
-    memset(prime, false, n * sizeof(bool));
+	bool *prime = malloc(n * sizeof(bool));
+	memset(prime, false, n * sizeof(bool));
 
-    if (n > 2) prime[2] = true;
-    if (n > 3) prime[3] = true;
+	if (n > 2)
+		prime[2] = true;
+	if (n > 3)
+		prime[3] = true;
 
-    for (int x = 1; x * x <= n; x++) {
-        for (int y = 1; y * y <= n; y++) {
-            int s = (4 * x * x) + (y * y);
-            if (s <= n && (s % 12 == 1 || s % 12 == 5))
-                prime[s] = !prime[s];
-            s = (3 * x * x) + (y * y);
-            if (s <= n && s % 12 == 7)
-                prime[s] = !prime[s];
-            s  = (3 * x * x) - (y * y);
-            if (x > y && s <= n && s % 12 == 11)
-                prime[s] = !prime[s];
-        }
-    }
+	for (int x = 1; x * x <= n; x++) {
+		for (int y = 1; y * y <= n; y++) {
+			int s = (4 * x * x) + (y * y);
+			if (s <= n && (s % 12 == 1 || s % 12 == 5))
+				prime[s] = !prime[s];
+			s = (3 * x * x) + (y * y);
+			if (s <= n && s % 12 == 7)
+				prime[s] = !prime[s];
+			s = (3 * x * x) - (y * y);
+			if (x > y && s <= n && s % 12 == 11)
+				prime[s] = !prime[s];
+		}
+	}
 
-    for (int i = 5; i * i <= n; i++) {
-        if (prime[i] == false) continue;
-        for (int j = i * i; j <= n; j += i * i) {
-            prime[j] = false;
-        }
-    }
+	for (int i = 5; i * i <= n; i++) {
+		if (prime[i] == false)
+			continue;
+		for (int j = i * i; j <= n; j += i * i) {
+			prime[j] = false;
+		}
+	}
 
-    for (int i = 2; i <= n; i++) {
-        if (prime[i])
-            result.data[result.size++] = i;
-    }
-    
-    return result;
+	for (int i = 2; i <= n; i++) {
+		if (prime[i])
+			result.data[result.size++] = i;
+	}
+
+	return result;
 }
 
 int_list prime_factor(int n) {
@@ -156,6 +161,36 @@ int_tuple_list twin_primes(int_list primes) {
 	return twins;
 }
 
+bool is_palindrome(int num) {
+	int reverse = 0;
+	int temp = num;
+	while (temp != 0) {
+		reverse = (reverse * 10) + (temp % 10);
+		temp /= 10;
+	}
+	return (reverse == num);
+}
+
+int prime_palindrome(int n) {
+    if (n <= 11) {
+        while (1) {
+            if (is_palindrome(n) && test_prime(n))
+                return n;
+            n++;
+        }
+    }
+	while (1) {
+		if (is_palindrome(n) && test_prime(n))
+			return n;
+		else
+			n++;
+        int digits = (int)log10(n) + 1;
+        if (digits % 2 == 0) {
+            n = (int)pow(10, digits);
+        }
+	}
+}
+
 int main(int argc, char **argv) {
 	int choice;
 	int n;
@@ -169,10 +204,11 @@ int main(int argc, char **argv) {
 	printf("5. Find prime gap merits of primes up to n\n");
 	printf("6. Find twin primes in primes up to n\n");
 	printf("7. Find all primes up to n with sieve of Atkin\n");
-	printf("8. Exit\n");
+	printf("8. Find all palindromic primes greater than or equal to n\n");
+	printf("9. Exit\n");
 
 	while (1) {
-		printf("\nEnter your choice (1-8): ");
+		printf("\nEnter your choice (1-9): ");
 		scanf("%d", &choice);
 
 		switch (choice) {
@@ -255,7 +291,7 @@ int main(int argc, char **argv) {
 			free(twins.data);
 			break;
 		}
-        case 7: {
+		case 7: {
 			printf("Enter a number: ");
 			scanf("%d", &n);
 			int_list primes = sieve_of_atkin(n);
@@ -268,7 +304,17 @@ int main(int argc, char **argv) {
 			free(primes.data);
 			break;
 		}
-		case 8:
+		case 8: {
+			printf("Enter a number: ");
+			scanf("%d", &n);
+			int result = prime_palindrome(n);
+			printf("The smallest palidromic prime greater than or equal to %d "
+			       "is: %d\n",
+			       n, result);
+			printf("\n");
+			break;
+		}
+		case 9:
 			return 0;
 		default:
 			printf("Invalid choice\n");
